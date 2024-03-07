608380458874f3872e0434622b26e53131b63785
MatheusLMMARCILIO <142438014+MatheusLMMARCILIO@users.noreply.github.com>
2024-03-07 16:38:07 -0300

    Create script.js

---
+++ splice/script.js
@@ -0,0 +1,10 @@
+const nomes = ['Maria', 'Jão', 'Eduardo', 'Gabriel', 'julia']
+
+const removidos = nomes.splice(-3,2)
+
+console.log(nomes, removidos)
+
+
+
+
+// Number.MAX_VALUE - tira do indice escolhido até o final da array
